6c8e3044d3769a2a4d9c9c5b99f61d4a3c6e9cd4
LeenuV <varshneyleena1996@gmail.com>
2020-01-12 22:01:10 +0530

    Third

--- src/app/components/schoolJagat/usersingle/usersingle.component.ts
+++ src/app/components/schoolJagat/usersingle/usersingle.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';  
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'usersingle',
   templateUrl: './usersingle.component.html',
-  styleUrls: ['./usersingle.component.css']
+  styleUrls: ['./usersingle.component.css'],
+  animations:[ 
+    trigger('fade',
+    [ 
+      state('void', style({ opacity : 0})),
+      transition(':enter',[ animate(300)]),
+      transition(':leave',[ animate(500)]),
+    ]
+)]
 })
 export class UsersingleComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) document) { }
 
   ngOnInit() {
   }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(e) {
+     if (window.pageYOffset > 550) {
+       let element = document.getElementById('navbar');
+       element.classList.add('sticky');
+     } else {
+      let element = document.getElementById('navbar');
+        element.classList.remove('sticky'); 
+     }
+  }
+
+
 
 }
